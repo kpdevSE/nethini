27d28115027334d767f7f08652ddc2eb170fa994
kpdevSE <kanishkapasindu6@gmail.com>
2024-05-07 09:38:49 +0530

    done

--- app/components/images.jsx
+++ app/components/images.jsx
@@ -8,10 +8,10 @@ export default function Images()
 {
     return (
         <div className="lg:grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 place-items-center place-content-center">
-            <Image src={imageOne} className="w-[220px] h-[300px] rounded-3xl lg:mt-0 mt-6  shadow-xl shadow-indigo-500 ..." />
-            <Image src={imageTwo} className="w-[300px] h-[200px] rounded-3xl lg:mt-0 mt-6  shadow-xl shadow-indigo-500 ..." />
-            <Image src={imageThree} className="w-[220px] h-[300px] rounded-3xl lg:mt-0 mt-6  shadow-xl shadow-indigo-500 ..." />
-            <Image src={imageFour} className="w-[300px] h-[200px] rounded-3xl lg:mt-0 mt-6  shadow-xl shadow-indigo-500 ..." />
+            <Image src={imageOne} alt="" className="w-[220px] h-[300px] rounded-3xl lg:mt-0 mt-6  shadow-xl shadow-indigo-500 ..." />
+            <Image src={imageTwo} alt="" className="w-[300px] h-[200px] rounded-3xl lg:mt-0 mt-6  shadow-xl shadow-indigo-500 ..." />
+            <Image src={imageThree} alt="" className="w-[220px] h-[300px] rounded-3xl lg:mt-0 mt-6  shadow-xl shadow-indigo-500 ..." />
+            <Image src={imageFour} alt="" className="w-[300px] h-[200px] rounded-3xl lg:mt-0 mt-6  shadow-xl shadow-indigo-500 ..." />
         </div>
     )
 }
--- app/components/thankyou.jsx
+++ app/components/thankyou.jsx
@@ -9,9 +9,9 @@ export default function ThankYou()
         <div className="flex flex-col items-start justify-between lg:flex-row ">
             <div className="flex flex-col-reverse lg:flex-col gap-10">
                 <p className="font-mono text-lg lg:w-[50%]">
-                    I never thought that I would be such a good friend to you.. But it happened.. I started talking to you because I can't live like other girls. Not every girl will come. Some people say that the reason for making this is because you like to get things like before. But now it has improved a bit.. So I decided to give this one.. I don't know if you will like it.
-
-                    Thank you very much for being so fit and close to me</p>
+                    I never thought that I would be such a good friend to you.. But it happened.. I started talking to you because I can&apos;t live like other girls. Not every girl will come. Some people say that the reason for making this is because you like to get things like before. But now it has improved a bit.. So I decided to give this one.. I don&apos;t know if you will like it.
+                    Thank you very much for being so fit and close to me
+                </p>
                 <div className="flex lg:items-start lg:justify-start w-full mt-4 gap-3 p-2 flex-col lg:flex-row items-center justify-center">
                     <Image src={BlueOne} alt="" className="lg:w-[330px] lg:h-[400px] rounded-3xl  shadow-xl shadow-indigo-500 ... w-[200px] h-[300px]" />
                     <Image src={Badminton} alt="" className="lg:w-[330px] lg:h-[400px] rounded-3xl  shadow-xl shadow-indigo-500 ... w-[200px] h-[300px]" />
