813c710ca2086b97df63b2b04343dd874d8e74b4
kpdevSE <kanishkapasindu6@gmail.com>
2024-05-07 09:29:36 +0530

    done

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Footer()
 {
     return (
@@ -8,7 +10,7 @@ export default function Footer()
                 <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <div className="sm:flex sm:items-center sm:justify-between">
                         <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                            <Image src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
                             <span className="self-center text-3xl  whitespace-nowrap dark:text-white font-mono font-bold">KPDEV</span>
                         </a>
                         <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
